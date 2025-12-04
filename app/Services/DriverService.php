6228365d61b69db2fc1fc55d798d0584f4fbf106
Maulina Sabrina <maulinasabrina04@gmail.com>
2025-12-05 02:20:20 +0700

    feat : setup all function  drivers service

---
+++ app/Services/DriverService.php
@@ -0,0 +1,42 @@
+<?php 
+namespace App\Services\Drivers;
+
+use App\Models\Driver;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use App\Repositories\Drivers\DriversRepository;
+
+class DriverService
+{
+    protected $repository;
+
+    public function __construct(DriversRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function getAllDrivers(int $perPage = 15): LengthAwarePaginator
+    {
+        return $this->repository->all($perPage);
+    }
+
+    public function getDriverById(int $id): ?Driver
+    {
+        return $this->repository->find($id);
+    }
+
+    public function createDriver(array $data): Driver
+    {
+        return $this->repository->create($data);
+    }
+
+    public function updateDriver(int $id, array $data): ?Driver
+    {
+        return $this->repository->update($id, $data);
+    }
+
+    public function deleteDriver(int $id): bool
+    {
+        return $this->repository->delete($id);
+    }
+}
+?>
