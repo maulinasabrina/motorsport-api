351c3dff6dfe40be7478ad50a1261addb8b0cf3d
Maulina Sabrina <maulinasabrina04@gmail.com>
2025-12-05 04:51:51 +0700

    feat : setup races route

--- routes/api.php
+++ routes/api.php
@@ -2,14 +2,18 @@
 
 use App\Http\Controllers\Api\Drivers\DriverController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Races\RaceController;
 
 
 Route::prefix('drivers')->group(function () {
+
     Route::get('/', [DriverController::class, 'index']);
     Route::get('/{id}', [DriverController::class, 'show']);
-        Route::post('/', [DriverController::class, 'store']);
-        Route::put('/{id}', [DriverController::class, 'update']);
-        Route::delete('/{id}', [DriverController::class, 'destroy']);
+    Route::post('/', [DriverController::class, 'store']);
+    Route::put('/{id}', [DriverController::class, 'update']);
+    Route::delete('/{id}', [DriverController::class, 'destroy']);
+
+
 
     //Protected routes
     // Route::middleware('auth:sanctum')->group(function () {
@@ -17,4 +21,15 @@ Route::prefix('drivers')->group(function () {
     //     Route::put('/{id}', [DriverController::class, 'update']);
     //     Route::delete('/{id}', [DriverController::class, 'destroy']);
     // });
+});
+
+Route::prefix('races')->group(function () {
+    Route::get('/', [RaceController::class,'index']);
+    Route::get('/{id}', [RaceController::class,'show']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/', [RaceController::class,'store']);
+        Route::put('/{id}', [RaceController::class,'update']);
+        Route::delete('/{id}', [RaceController::class,'destroy']);
+    });
 });
